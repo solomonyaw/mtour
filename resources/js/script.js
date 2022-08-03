acf39aefc5891bd9ed1fdb79f5d43823bb807fdb
solomonyaw <s.adeklo@st.vvu.edu.gh>
2022-08-02 17:00:02 -0700

    updated sass files

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,103 +1,103 @@
 
-// // scroll on buttons
-  $(document).ready(function(){
-    $('.js--discover').click(function(e){      var linkHref = $(this).attr('href');
-     $('html,body').animate({
-        scrollTop: $('.section-tours').offset().top},1000);
+// // // scroll on buttons
+//   $(document).ready(function(){
+//     $('.js--discover').click(function(e){      var linkHref = $(this).attr('href');
+//      $('html,body').animate({
+//         scrollTop: $('.section-tours').offset().top},1000);
     
-         e.preventDefault();
+//          e.preventDefault();
 
-     });
+//      });
 
-     $('.item2').click(function(e){
-         var linkHref = $(this).attr('href');
-         $('html,body').animate({
-           scrollTop: $('.features_section').offset().top},1000);
-            e.preventDefault();
+//      $('.item2').click(function(e){
+//          var linkHref = $(this).attr('href');
+//          $('html,body').animate({
+//            scrollTop: $('.features_section').offset().top},1000);
+//             e.preventDefault();
    
-        });
+//         });
        
-       //navigation scroll 
-     $('.item1').click(function(e){
-         var linkHref = $(this).attr('href');
-         $('html,body').animate({
-           scrollTop: $('.section-about').offset().top},1000);
+//        //navigation scroll 
+//      $('.item1').click(function(e){
+//          var linkHref = $(this).attr('href');
+//          $('html,body').animate({
+//            scrollTop: $('.section-about').offset().top},1000);
        
-            e.preventDefault();
+//             e.preventDefault();
    
-        });
+//         });
 
-        $('.item3').click(function(e){
-         var linkHref = $(this).attr('href');
-         $('html,body').animate({
-           scrollTop: $('.section-tours').offset().top},1000);
+//         $('.item3').click(function(e){
+//          var linkHref = $(this).attr('href');
+//          $('html,body').animate({
+//            scrollTop: $('.section-tours').offset().top},1000);
        
-            e.preventDefault();
+//             e.preventDefault();
    
-        });
+//         });
 
-        $('.item4').click(function(e){
-         var linkHref = $(this).attr('href');
-         $('html,body').animate({
-           scrollTop: $('.section-stories').offset().top},1000);
+//         $('.item4').click(function(e){
+//          var linkHref = $(this).attr('href');
+//          $('html,body').animate({
+//            scrollTop: $('.section-stories').offset().top},1000);
        
-            e.preventDefault();
+//             e.preventDefault();
    
-        });
-        $('.item5').click(function(e){
-         var linkHref = $(this).attr('href');
-         $('html,body').animate({
-           scrollTop: $('.section-book').offset().top},1000);
+//         });
+//         $('.item5').click(function(e){
+//          var linkHref = $(this).attr('href');
+//          $('html,body').animate({
+//            scrollTop: $('.section-book').offset().top},1000);
        
-            e.preventDefault();
+//             e.preventDefault();
    
-        });
+//         });
 
-        $('.btn-text').click(function(e){
-          var linkHref = $(this).attr('href');
-          $('html,body').animate({
-            scrollTop: $('.section-tours').offset().top},1000);
+//         $('.btn-text').click(function(e){
+//           var linkHref = $(this).attr('href');
+//           $('html,body').animate({
+//             scrollTop: $('.section-tours').offset().top},1000);
         
-             e.preventDefault();
+//              e.preventDefault();
     
-         });
+//          });
 
-        btn-text
+        
 
-//        //mobile nav drop-down
-//       //  $('.navigation__icon').click(function(){
-//       //   var nav = $('.navigation__nav');
-//       //   var icon =$('.navigation__icon a');
-//       //   nav.slideToggle(200);
+// //        //mobile nav drop-down
+// //       //  $('.navigation__icon').click(function(){
+// //       //   var nav = $('.navigation__nav');
+// //       //   var icon =$('.navigation__icon a');
+// //       //   nav.slideToggle(200);
 
-//       //   if(icon.hasClass('ion-navicon-round'))
-//       //   {
-//       //      icon.addClass('ion-close-round');
-//       //      icon.removeClass('ion-navicon-round');
-//       //   }
-//       //   else{
-//       //       icon.addClass('ion-navicon-round');
-//       //       icon.removeClass('ion-close-round');
-//       //   }
+// //       //   if(icon.hasClass('ion-navicon-round'))
+// //       //   {
+// //       //      icon.addClass('ion-close-round');
+// //       //      icon.removeClass('ion-navicon-round');
+// //       //   }
+// //       //   else{
+// //       //       icon.addClass('ion-navicon-round');
+// //       //       icon.removeClass('ion-close-round');
+// //       //   }
 
-//       //  })
+// //       //  })
 
       
- });
+//  });
 
-// // var vid = document.getElementById("myvideo");
+// // // var vid = document.getElementById("myvideo");
 
-// // function playVid()
-// // {
-// //   vid.play();
-// // }
-// // function pauseVid()
-// // {
-// //   vid.pause();
-// // }
+// // // function playVid()
+// // // {
+// // //   vid.play();
+// // // }
+// // // function pauseVid()
+// // // {
+// // //   vid.pause();
+// // // }
 
-// // playVid();
-// // pauseVid();
+// // // playVid();
+// // // pauseVid();
 
 
 
